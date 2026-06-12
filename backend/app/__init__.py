e6a74cbbbd0327e2d8a8972d59a8c5cfb641c33c
MakisVarv <christomvarv@yahoo.com>
2026-06-12 15:07:03 +0300

    create contacts

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
-
+from app import models
+from app.config import Config
+from app.extensions import db, migrate
 from app.routes.health_routes import health_bp
 from app.routes.contact_routes import contact_bp
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
     CORS(app)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app import models
 
     app.register_blueprint(health_bp)
     app.register_blueprint(contact_bp)
